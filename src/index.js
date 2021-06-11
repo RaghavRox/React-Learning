dc3247ca8765e01c93d4339ad32cd35419f15124
RaghavRox <raghavrox2002@gmail.com>
2021-06-11 23:19:55 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+function Nadi(props){
+  return <h1> {props.needi}</h1>;
+}
+
+const element = <Nadi needi = "wtf"/>;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
