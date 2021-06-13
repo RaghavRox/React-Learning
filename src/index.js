2d7ee0f7a4078809bf99381d576183252d0d039f
RaghavRox <raghavrox2002@gmail.com>
2021-06-13 13:57:10 +0530

    See ur face

--- src/index.js
+++ src/index.js
@@ -1,55 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 
-class Wa extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {number : null};
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+// class Wa extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {number : null};
 
-  }
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+
+//   }
 
   
 
-  handleSubmit(event){
-    window.location.href = "https://wa.me/91"+this.state.number;
-    event.preventDefault();
-  } 
+//   handleSubmit(event){
+//     window.location.href = "https://wa.me/91"+this.state.number;
+//     event.preventDefault();
+//   } 
 
-  handleChange(event){
-    this.setState({number : event.target.value});
+//   handleChange(event){
+//     this.setState({number : event.target.value});
+//   }
+
+//   render(){
+//     return (
+//       <form onSubmit = {this.handleSubmit}>
+//           <label>
+//             number : <input type ="text" value = {this.state.number} onChange = {this.handleChange}/>
+//           </label>
+//           <input type = "submit" value = "chat in whatsapp"/>
+//       </form>
+//     );
+//   }
+// }
+
+
+
+let localStream = null;
+
+class MyVideo extends React.Component{
+  constructor(props){
+    super(props);
+    this.startStream = this.startStream.bind(this);
+    this.vidRef = React.createRef();
+  }
+
+  async startStream(){
+    localStream = await navigator.mediaDevices.getUserMedia({
+      video : true,
+      audio : false
+    });
+    this.vidRef.current.srcObject = localStream;
+    this.vidRef.current.play();
   }
 
   render(){
-    return (
-      <form onSubmit = {this.handleSubmit}>
-          <label>
-            number : <input type ="text" value = {this.state.number} onChange = {this.handleChange}/>
-          </label>
-          <input type = "submit" value = "chat in whatsapp"/>
-      </form>
+    return(
+      <div>
+        <video ref={this.vidRef} width="360" height ="240" ></video>
+        <button onClick ={this.startStream} >Start webcam</button>
+      </div>
     );
   }
 }
 
 
+
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <Wa />,
+  <MyVideo />,
   document.getElementById('root')
 );
 
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
